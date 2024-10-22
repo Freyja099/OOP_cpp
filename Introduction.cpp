cec6e99ddf314eaf8f908cebfdbd5c30d2101670
Aya Idrissi <aya.zoujari.id@gmail.com>
2024-10-22 19:38:47 +0100

    m1

--- Introduction.cpp
+++ Introduction.cpp
@@ -1,1 +1,22 @@
 // * Session 1 : Introduction to OOP : Object Oriented Programming in c++
+
+// * OOP is programmig paradigm which means it is a set of rules, ideas and concepts
+// * it's basically a standard in programming that we use to solve a specific type of problem
+// * The idea behind object-oriented paradigm is we want to be able to represent real life objects/entities
+// * together with their characteristics, attributes, behaviors ; is more like we want to explain those objects
+// * to our computer and represents them in our program
+
+// * For example a car : car as an entity in real life has many different attributs some of those are manufacturer of that car
+// * (mercedes, bmw, audi ...) then that car certainly has a colour, price,... those would be the attributes and characteristics of 
+// * that car in real life ; Some of the behavior of the car is that a car can drive, it can accelerate, stop, open the door, tank the car
+// * to put fuel,... ; 
+
+// * To represent those entities in our program we use classes which is a very important concept in OOP 
+// * Class is building block of object oriented programming, it's basically user-defined data type.
+// * In order to understand what is user-defined data types we have to first understand predefined data types 
+// * or built-in data types (data types like integer, float, double,...) 
+// * we can consider that a class is a little bit more complexe data types 
+// * for example if we want storing username in our program we can declare the variable as a string and store it but what if we wanted to store
+// * the entire user that contains username and other characteristics (name, email, gender, height,...) this group of variable that represent a user 
+// * is what will give us a class 
+// * We should store information that are important for our program.
